0481dcbf7dabfa5d71e6fdd60c67e81f94203879
Austen Querino <austen.querino@gmail.com>
2023-03-15 00:25:08 -0400

    add pure Django forms

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,13 +1,13 @@
-
 from django import forms
 from .models import Profile
 
 class ProfileCreateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = [
-            'username',
-            'name',
-            'bio', 
-        ]
+        fields = '__all__'
+        exclude = ['created_at', 'updated_at']
 
+class RawProfileForm(forms.Form):
+    username = forms.CharField()
+    name = forms.CharField()
+    
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .forms import ProfileCreateForm
+from .forms import ProfileCreateForm, RawProfileForm
 from .models import Profile
 
 # Create your views here.
@@ -12,12 +12,35 @@ def profile_detail_view(request):
     return render(request, "profiles/profile_detail.html", context)
 
 def profile_create_view(request):
-    form = ProfileCreateForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-
+    my_form = RawProfileForm()
+    if request.method == "POST":
+        my_form = RawProfileForm(request.POST)
+        if my_form.is_valid():
+            # now the data is good
+            print(my_form.cleaned_data)
+            Profile.objects.create(**my_form.cleaned_data)
+        else:
+            print(my_form.errors)
     context = {
-        'form': form 
+        "form": my_form
     }
     return render(request, "profiles/profile_create.html", context)
+
+# def profile_create_view(request):
+#     print(request.POST)
+#     my_new_title = request.POST.get('title')
+#     # Profile.objects.create(title=my_new_title)
+#     context = {}
+#     return render(request, "profiles/profile_create.html", context)
+
+# def profile_create_view(request):
+#     form = ProfileCreateForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProfileCreateForm()
+
+#     context = {
+#         'form': form 
+#     }
+#     return render(request, "profiles/profile_create.html", context)
     
