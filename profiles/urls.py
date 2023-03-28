9204187aa88f770c481fae8c4981ffef11afb307
Austen Querino <austen.querino@gmail.com>
2023-03-28 00:07:57 -0400

    class based view - DetailView

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,6 +5,7 @@ from .views import (
     profile_create_view, 
     profile_update_view,
     profile_detail_view, 
+    ProfileDetailView,
     profile_delete_view,
 )
 
@@ -13,7 +14,8 @@ urlpatterns = [
     # path('', profile_list_view, name='profile-list'),
     path('', ProfileListView.as_view(), name='profile-list'),
     path('create/', profile_create_view, name='profile-create'),
-    path('<int:id>/', profile_detail_view, name='profile-detail'),
+    # path('<int:id>/', profile_detail_view, name='profile-detail'),
+    path('<int:pk>/', ProfileDetailView.as_view(), name='profile-detail'),
     path('<int:id>/update/', profile_update_view, name='profile-update'),
     path('<int:id>/delete/', profile_delete_view, name='profile-delete'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -66,3 +66,13 @@ class ProfileListView(ListView):
     template_name = "profiles/profile_list_link.html"
     queryset = Profile.objects.all()
 
+
+class ProfileDetailView(DetailView):
+    template_name = "profiles/profile_detail.html"
+    queryset = Profile.objects.all()
+
+    # def get_object(self):
+    #     id_ = self.kwargs.get("id")
+    #     return get_object_or_404(Profile, id=id_)
+
+
